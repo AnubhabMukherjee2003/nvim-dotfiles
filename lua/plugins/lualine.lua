39ffd3bf423320f2e9b699985c126100ba8a9a81
AnubhabMukherjee2003 <antucruse621@gmail.com>
2026-02-06 09:16:00 +0530

    buffeline added

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,6 +4,16 @@ return {
     dependencies = {
       "nvim-tree/nvim-web-devicons",
     },
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "auto",      -- adapts to terminal/colorscheme
+          icons_enabled = true,
+          section_separators = '',
+          component_separators = '',
+        },
+      })
+    end,
   },
 }
 
